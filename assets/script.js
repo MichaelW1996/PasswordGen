447a18b97aa992c49e3c936fe88c1a89d475ad73
Michael Walters <michael1996walters@gmail.com>
2023-02-11 14:45:27 +0000

    added logic for random password generation

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,13 @@
-//Characters
-var lower = 'abcdefghijklmnopqrstuvwxyz'.split("");
-var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
-var num = [1,2,3,4,5,6,7,8,9,0];
-var spec ='!"#$%&()*+,-./:;<>+?[,]^_`{}|~'.split("");
-var chars =[]
-
-console.log(lower)
-console.log(upper)
-console.log(num)
-console.log(spec)
+//Characters to be used
+var lower = 'abcdefghijklmnopqrstuvwxyz'.split("");//lowercase alphabet split into an array using .split method
+var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");//Uppercase alphabet split into an array using .split method
+var num = [1,2,3,4,5,6,7,8,9,0];//Number array  
+var spec ='!"#$%&()*+,-./:;<>+?[,]^_`{}|~'.split("");//special characters the user may want in password,split int an array using .split method
+var chars =[] //working character array, begining empty, to be have others concat'ed into it
+var passMax = 128 //maximum password length
+var passMin = 8 //minimum password length
+var length //variable to be used for password length
+var pass =""//string for password
 
 // Assignment Code
 // var generateBtn = document.querySelector("#generate");
@@ -25,33 +24,72 @@ console.log(spec)
 // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
 
-var charset = function(){
-  if(confirm("Lower Characters?")==true){
-    chars = chars.concat(lower)
-  }
-  if(confirm("Upper Characters?")==true){
-    chars = chars.concat(upper)
-  }
-  if(confirm("Numbers?")==true){
-    chars = chars.concat(num)
-  }
-  if(confirm("Special Characters?")==true){
-    chars = chars.concat(spec)
+
+
+var passlength = function(){ //function to ask user for a password length
+  let Plength= prompt("How long of a password would you like?") //input from user
+  length = parseInt(Plength) //converts input to a number, non numbers return as "NaN" or Not a Number
+  if(isNaN(length)){ // if input returns as not a number
+    alert("Please enter a number") //alerts user of issue
+    passlength() // restarts the function
+  } else { // if input returns as a number
+    if(length < passMin || length > passMax){ // checks if the number is too long or too short
+      alert("Password length must be between " + passMin + " & " + passMax + " characters") // alerts user to the need to adjust password length
+      passlength() // restarts function
+    } else { //if input is a number between min and max password length
+      console.log("Length locked in")
+      return generate()
+    }
   }
 }
 
-charset()
-if(chars.length==0){
-  console.log("Stupid boy, try again")
-}else{
-  console.log("Well done")
-}
-console.log(chars)
+var charCheck = function(){ //checks the user has added characters to the working character array 
+  if(chars.length==0){ //If the working character array is empty
+    alert("You must choose some characters for your password") //alerts user that the working array is empty
+    charset() //restarts the function to select character to be used
+  }else{ //if the working character array is not empty
+    passlength() //runs function to request a password lenght
+    console.log("Characters selected")
+  }}
 
+var charset = function(){ // function to ask user which characters to be used
+  if(confirm("Lower Characters?")==true){ //would you like to use lowercase letters?
+    chars = chars.concat(lower) //adds lower case letters to working character list
+  }
+  if(confirm("Upper Characters?")==true){//would you like to use upper case letters?
+    chars = chars.concat(upper) //adds Upper case letters to working character list
+  }
+  if(confirm("Numbers?")==true){ //as above, but with numbers
+    chars = chars.concat(num)//adds numbers to working character list 
+  }
+  if(confirm("Special Characters?")==true){//as above, with special characters
+    chars = chars.concat(spec) //adds special characters to working list
+  }
+  charCheck()//Runs function to check the working list 
+}
+
+
+
+var random = function(x){
+  return(
+    Math.floor(Math.random()* x)
+  )
+
+}
+
+var generate = function(){
+  for(let i=0; i<length; i++){
+    pass=pass+chars[random(chars.length)]
+    console.log(pass)
+  }
+}
+
+
+charset() //test function, runs the charset function on reload 
 // var generatePassword = function(){
-  //run charset()
-  //Select types of characters
-  //select lenght
-  //for loop with array of characters to pick from
+  //run charset() yep
+  //Select types of characters yep
+  //select lenght yep
+  //for loop with array of characters to pick from NOPE
   //return password
 // }
